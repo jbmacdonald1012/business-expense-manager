5268736f1af5cbc4fc984e8fda26a61abf263106
jbmacdonald1012 <jbmacdonald1012@gmail.com>
2025-04-09 21:41:38 -0600

    fix: update start script path and improve dotenv configuration handling

--- dist/src/server.js
+++ dist/src/server.js
@@ -3,9 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-process.env.NODE_ENV === 'production' ? dotenv_1.default.config() : dotenv_1.default.config({ path: './local.env' });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+process.env.NODE_ENV === 'production' ? dotenv_1.default.config() : dotenv_1.default.config({ path: './local.env' });
 const app_1 = __importDefault(require("./app"));
 const db_1 = require("./config/db");
 const PORT = process.env.PORT || 3000;
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,16 @@
-process.env.NODE_ENV === 'production' ? dotenv.config() : dotenv.config({ path: './local.env' });
 import dotenv from 'dotenv';
-
-
 dotenv.config();
+
+process.env.NODE_ENV === 'production' ? dotenv.config() : dotenv.config({ path: './local.env' });
 
 import app from './app';
 import { connectToDatabase } from './config/db';
 
-
 const PORT = process.env.PORT || 3000;
 
 (async () => {
-    await connectToDatabase();
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 })();
